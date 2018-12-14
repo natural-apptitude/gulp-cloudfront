2018c7adb97122765aadd623f04937db434798d4
Joe Woodhouse <joewoodhouse30@gmail.com>
2018-12-14 12:56:21 +0000

    Seems to cause a crash on our deployment, removing seems fine, so removed

--- tool.js
+++ tool.js
@@ -31,11 +31,6 @@ module.exports = function(options) {
                     data.DistributionConfig.Logging.Prefix = '';
                 }
 
-                // Causing problems on a default cloudfront setup, why is this needed?
-                if (data.DistributionConfig.Origins.Items instanceof Array && data.DistributionConfig.Origins.Items[0].S3OriginConfig.OriginAccessIdentity === null) {
-                    data.DistributionConfig.Origins.Items[0].S3OriginConfig.OriginAccessIdentity = '';
-                }
-
                 if (data.DistributionConfig.DefaultRootObject === defaultRootObject.substr(1)) {
                     gutil.log('gulp-cloudfront:', "DefaultRootObject hasn't changed, not updating.");
                     return deferred.resolve();
